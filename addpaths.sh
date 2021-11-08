959820d694057ccf08ce037b8f144071fa8c0696
Nate C-K <nculwell@gmail.com>
2021-11-07 20:51:17 -0600

    Applesoft generates correctly.

--- addpaths.sh
+++ addpaths.sh
@@ -1,9 +1,10 @@
-DIR="$(cd "$(dirname -- "$0")" && pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OLDPATH=$(mktemp)
 echo ":$PATH:" > "$OLDPATH"
 for d in apple2 c64 emu6502 ; do
   p="$DIR/$d"
   if ! fgrep -q ":$p:" "$OLDPATH" ; then
+    echo Adding path: "$p"
     export PATH="$PATH:$p"
   fi
 done
